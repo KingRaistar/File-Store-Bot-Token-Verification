2b1a083b01b53a85941b6a6068a656bd53645f01
KingRaistar <166215350+KingRaistar@users.noreply.github.com>
2024-04-22 08:58:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "moneycase.link")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "f5414ca5987f6ae8600a8239fc9535054adad30f")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
-IS_VERIFY = os.environ.get("IS_VERIFY", "True")
+IS_VERIFY = os.environ.get("IS_VERIFY", "False")
 TUT_VID = os.environ.get("TUT_VID","https://t.me/How_to_Download_7x/32")
 
 #force sub channel id, if you want enable force sub
